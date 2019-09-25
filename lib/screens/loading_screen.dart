0c59bcf7aaa86b6936f6efab6acecd9d354a5cb0
Varuni-Punchihewa <1995varuni@gmail.com>
2019-09-25 23:05:24 +0530

    track user's current location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
 import 'package:sl_travel_app/services/location.dart';
+import 'dart:math';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,8 +10,11 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  static double latitude;
-  static double longitude;
+  double latitude;
+  double longitude;
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  int _markerIdCounter = 1;
+  MarkerId selectedMarker;
 
   @override
   void initState() {
@@ -23,8 +27,17 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
     latitude = location.latitude;
     longitude = location.longitude;
-    print(latitude);
-    print(longitude);
+    print('Current latitude: $latitude');
+    print('Current Longitude: $longitude');
+    //_add();
+
+    setState(() {
+      location.positionChanged();
+      latitude = location.latitude;
+      longitude = location.longitude;
+      print('After tacking latitude: $latitude');
+      print('After tracking longitude: $longitude');
+    });
   }
 
   Completer<GoogleMapController> _controller = Completer();
@@ -34,6 +47,51 @@ class _LoadingScreenState extends State<LoadingScreen> {
       target: LatLng(37.43296265331129, -122.08832357078792),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
+
+  void _add() {
+    //final int markerCount = markers.length;
+
+    final String markerIdVal = 'marker_id_$_markerIdCounter';
+    _markerIdCounter++;
+    final MarkerId markerId = MarkerId(markerIdVal);
+    LatLng center = LatLng(latitude, longitude);
+
+    Marker marker = Marker(
+      markerId: markerId,
+      position: LatLng(
+        center.latitude + sin(_markerIdCounter * pi / 6.0) / 20.0,
+        center.longitude + cos(_markerIdCounter * pi / 6.0) / 20.0,
+      ),
+      infoWindow: InfoWindow(title: markerIdVal, snippet: '*'),
+      onTap: () {
+        _onMarkerTapped(markerId);
+      },
+    );
+
+    setState(() {
+      markers[markerId] = marker;
+    });
+  }
+
+  void _onMarkerTapped(MarkerId markerId) {
+    final Marker tappedMarker = markers[markerId];
+    if (tappedMarker != null) {
+      setState(() {
+        if (markers.containsKey(selectedMarker)) {
+          final Marker resetOld = markers[selectedMarker]
+              .copyWith(iconParam: BitmapDescriptor.defaultMarker);
+          markers[selectedMarker] = resetOld;
+        }
+        selectedMarker = markerId;
+        final Marker newMarker = tappedMarker.copyWith(
+          iconParam: BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueGreen,
+          ),
+        );
+        markers[markerId] = newMarker;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,6 +129,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
               _controller.completeError(error);
             }
           },
+          markers: Set<Marker>.of(markers.values),
         ),
       ),
     );
--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,17 +1,40 @@
+import 'dart:async';
 import 'package:geolocator/geolocator.dart';
 
 class Location {
   double latitude;
   double longitude;
+  var geolocator = Geolocator();
 
   Future<void> getCurrentLocation() async {
     try {
-      Position position = await Geolocator().getCurrentPosition(
+      Position position = await geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.bestForNavigation);
       latitude = position.latitude;
       longitude = position.longitude;
     } catch (e) {
       print(e);
     }
+  }
+
+  void positionChanged() {
+    var locationOptions = LocationOptions(
+        accuracy: LocationAccuracy.bestForNavigation, distanceFilter: 1);
+
+    // ignore: cancel_subscriptions
+    StreamSubscription<Position> positionStream = geolocator
+        // ignore: cancel_subscriptions
+        .getPositionStream(locationOptions)
+        .listen((Position position) {
+      if (position == null) {
+        print('Unkonown');
+      } else {
+        latitude = position.latitude;
+        longitude = position.longitude;
+        print('--------------------------');
+        print('Latitude changed: $latitude');
+        print('Longitude changed: ${position.longitude.toString()}');
+      }
+    });
   }
 }
